ac456dc274305a63cfc6a9850d0d7ef8f2a859c9
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-05-18 20:39:57 -0600

    added stuff from new check to here cause laziness

--- sqlqueries/db_functionsquery.sql
+++ sqlqueries/db_functionsquery.sql
@@ -2,21 +2,21 @@ insert into dealership (name, location, date_established) values ('adealer', 'Ed
 insert into dealership (name, location, date_established) values ('adealer2', 'Edmonton', '2016-09-14');
 insert into dealership (name, location, date_established) values ('adealer2', 'Edmonton', '2011-06-17');
 
-insert into for_sale_cars (dealership_id, price, model, `year`) values (2, 1400, 'sunfire', '2022-01-01'),
-(2, 1400, 'sunfire', '2022-01-02'),
-(1, 1400, 'sunfire', '2022-01-03'),
-(3, 1400, 'sunfire', '2022-01-04'),
-(1, 1400, 'sunfire', '2022-01-05'),
-(1, 1400, 'sunfire', '2022-01-06'),
-(2, 1400, 'sunfire', '2022-01-07'),
-(1, 1400, 'sunfire', '2022-01-08'),
-(2, 1400, 'sunfire', '2022-01-09'),
-(3, 1400, 'sunfire', '2022-01-10'),
-(1, 1400, 'sunfire', '2022-01-11'),
-(2, 1400, 'sunfire', '2022-01-12'),
-(1, 1400, 'sunfire', '2022-01-13'),
-(3, 1400, 'sunfire', '2022-01-14'),
-(1, 1400, 'sunfire', '2022-01-15');
+insert into for_sale_cars (dealership_id, price, model, `year`) values (2, 1400, 'car', '2022-01-01'),
+(2, 1400, 'car', '2022-01-02'),
+(1, 1400, 'jeep', '2022-01-03'),
+(3, 1400, 'car', '2022-01-04'),
+(1, 1400, 'jeep', '2022-01-05'),
+(1, 1400, 'car', '2022-01-06'),
+(2, 1400, 'truck', '2022-01-07'),
+(1, 1400, 'car', '2022-01-08'),
+(2, 1400, 'truck', '2022-01-09'),
+(3, 1400, 'car', '2022-01-10'),
+(1, 1400, 'van', '2022-01-11'),
+(2, 1400, 'car', '2022-01-12'),
+(1, 1400, 'SUV', '2022-01-13'),
+(3, 1400, 'car', '2022-01-14'),
+(1, 1400, 'trailer', '2022-01-15');
 
 
 
@@ -43,4 +43,10 @@ group by dealership_id;
 
 select model, count(id) from for_sale_cars 
 group by dealership_id
-having count(id) >=6;
+having count(id) >=2;
+
+select model, count(id) from for_sale_cars 
+WHERE `year` > '2020-01-01' and model LIKE ('%truck%')
+group by dealership_id
+having count(id) >=2 ;
+
